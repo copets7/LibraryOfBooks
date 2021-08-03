11a24641efee46c66d67c10574f36a8e50fb8d81
Andrey Yarosh <andreyarosh7@gmail.com>
2021-08-03 14:13:27 +0300

    add methods

--- src/main/java/Run.java
+++ src/main/java/Run.java
@@ -1,15 +1,30 @@
 import controller.UserController;
 import controller.impl.BookControllerImpl;
 import controller.impl.UserControllerImpl;
+import dao.impl.DAOConnection;
 import dao.impl.UserDAOImpl;
 import entiti.Book;
 import entiti.User;
 
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Run {
-    public static void main(String[] args) throws IOException {
 
+    public static void main(String[] args) throws IOException, SQLException {
+        DAOConnection daoConnection = new DAOConnection();
+
+        String query = "select * from type_genre";
+
+        Statement statement = daoConnection.getConnection().createStatement();
+        ResultSet resultSet = statement.executeQuery(query);
+        while (resultSet.next()){
+            int id = resultSet.getInt("id");
+            String name = resultSet.getString("genre_name");
+            System.out.println(id + " : " + name);
         }
-
+    }
 }
--- src/main/java/dao/impl/DAOConnection.java
+++ src/main/java/dao/impl/DAOConnection.java
@@ -8,13 +8,17 @@ import java.sql.*;
 public class DAOConnection {
     public static final String NAME_USER = "root";
     public static final String PASSWORD = "70286CopetS";
-    public static final String URL = "jdbc:mysql://localhost:3306/library";
+    public static final String URL = "jdbc:mysql://localhost:3306/myLibrary";
     public static Connection connection;
     public static Statement statement;
 
+    public static Connection getConnection() {
+        return connection;
+    }
     static {
         try {
             connection = DriverManager.getConnection(URL,NAME_USER,PASSWORD);
+            System.out.println("Подключено");
         }catch (SQLException throwables){
             throwables.printStackTrace();
             throw new RuntimeException();
@@ -28,41 +32,5 @@ public class DAOConnection {
             throw new RuntimeException();
         }
     }
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
 
-        try {
-
-            Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
-
-        } catch (ClassNotFoundException | NoSuchMethodException e) {
-
-            System.out.println("Where is your MySQL JDBC Driver?");
-            e.printStackTrace();
-            return;
-
-        } catch (InvocationTargetException e) {
-            e.printStackTrace();
-        } catch (InstantiationException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        }
-
-        statement.execute("INSERT INTO user (user_name) VALUES ('Andrey')");
-
-
-        ResultSet resultSet = statement.executeQuery("select\n" +
-                "    u.user_name\n" +
-                "from\n" +
-                "    `user` u;");
-
-        while (resultSet.next()){
-            System.out.println(resultSet.getString(1));
-
-        }
-      connection.close();
-
-      /*  PreparedStatement st = connection.prepareStatement("'Andrey'");
-        st.executeUpdate();*/
-    }
 }
